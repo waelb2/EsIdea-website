856322586c439e6de4bb5065cd923cf1babd06ed
bouguessa wail <105824672+waelb2@users.noreply.github.com>
2024-03-08 12:20:49 +0100

    dist folder updated

---
+++ server/dist/src/user/userModels.js
@@ -0,0 +1,71 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserRole = exports.User = void 0;
+const mongoose_1 = __importStar(require("mongoose"));
+const isEmail_1 = __importDefault(require("validator/es/lib/isEmail"));
+var UserRole;
+(function (UserRole) {
+    UserRole["ADMIN"] = "admin";
+    UserRole["USER"] = "user";
+})(UserRole || (exports.UserRole = UserRole = {}));
+const userSchema = new mongoose_1.Schema({
+    firstName: {
+        type: String,
+        required: [true, "First name is required"]
+    },
+    lastName: {
+        type: String,
+        required: [true, "Last name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        validate: [isEmail_1.default, "Enter a valid email"]
+    },
+    password: {
+        type: String,
+    },
+    profilePicUrl: {
+        type: String,
+    },
+    role: {
+        type: String,
+        enum: Object.values(UserRole),
+        default: UserRole.USER
+    },
+    joinDate: {
+        type: Date,
+        required: [true, "User joining date is required"]
+    },
+    //notifications: [{ type: Schema.Types.ObjectId, ref: 'Notification' }],
+    // projects : [{ type: Schema.Types.ObjectId, ref: 'Project' }], 
+    // invitations :    // projects : [{ type: Schema.Types.ObjectId, ref: 'Invitation' }], 
+});
+const User = mongoose_1.default.model('User', userSchema);
+exports.User = User;
