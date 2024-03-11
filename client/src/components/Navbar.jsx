c2edb4329c98e2cccfea3f85dae6516e35efcd8b
Taleb Mohamed Adnane <lm_taleb@esi.dz>
2024-03-11 22:35:16 +0600

    Frontend init

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'; // Importing React library
+import Logo from '../assets/logo.png'; // Importing the logo image
+
+// Defining a functional component named Navbar
+const Navbar = () => {
+  return (
+    // Navbar container
+    <nav className='w-full px-7 pt-3 items-center flex justify-between align-middle'>
+      {/* Logo */}
+      <a href="#">
+        <img className='w-[124px] h-[32px]' src={Logo} alt="ESIdea_logo" />
+      </a>
+
+      {/* Navigation links */}
+      <ul className='flex items-center'>
+        {/* Log in link */}
+        <li>
+          <a className='text-[#59AEF8] font-semibold' href="#">Log in</a>
+        </li>
+        {/* Sign up link */}
+        <li>
+          <a className='bg-[#59AEF8] ml-5 px-8 py-2 text-white font-semibold rounded-full'  href="#">Sign up</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+// Exporting the Navbar component as default
+export default Navbar;
---
+++ client/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3A86FF",
+        skyBlue: "#59AEF8",
+        black: "#1C1C1E",
+        grey: "#55555E",
+        white:"#FEFEFE",
+        red:"#EA3323"
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
