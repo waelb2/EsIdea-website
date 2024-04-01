b687f9db25655e3198e276155d74619efb304d25
bouguessa wail <105824672+waelb2@users.noreply.github.com>
2024-04-01 11:58:34 +0100

    solving some conflicts

--- server/server.ts
+++ server/server.ts
@@ -1,28 +1,32 @@
-<<<<<<< HEAD
-import express, { Express, Request, Response } from 'express'
+import express, { Express } from 'express'
 import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
+import passport from 'passport'
+import './src/config/passport-setup'
 import { connectDB } from './src/config/db'
-=======
-import express, { Express } from "express";
-import dotenv from "dotenv";
-import passport from 'passport';
-import "./src/config/passport-setup";
-import { connectDB } from "./src/config/db";
-import session from 'express-session';
-import bodyParser from "body-parser";
-import routes from "./routes";
->>>>>>> 95d72a3a662ffa28d3e5d64044a290121de16b23
+import session from 'express-session'
+import bodyParser from 'body-parser'
 
-const app: Express = express();
-dotenv.config();
+dotenv.config()
 
 // Configuring the host
-<<<<<<< HEAD
 const HOST = process.env.HOST
 const PORT = process.env.PORT || 3000
 
 const app: Express = express()
+
+// app config
+
+app.use(
+  session({
+    secret: 'secret_key',
+    resave: false,
+    cookie: { secure: false, maxAge: 30 * 24 * 60 * 60 * 1000 },
+    saveUninitialized: false
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -30,10 +34,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 import routes from './routes'
 
 app.use(routes)
+
 const start = async () => {
   try {
     await connectDB(String(process.env.DATABASE_URI))
     console.log('DATABASE CONNECTED')
+
     app.listen(PORT, () => {
       console.log(`Server starting at http://localhost:${PORT}`)
     })
@@ -43,36 +49,3 @@ const start = async () => {
 }
 
 start()
-=======
-const PORT = process.env.PORT || 3000;
-
-app.use(session({
-    secret: 'secret_key',
-    resave: false,
-    cookie :{secure:false,maxAge:30*24*60*60*1000},
-    saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(routes);
-
-const start = async () => {
-    try {
-        await connectDB(String(process.env.DATABASE_URI));
-        console.log("DATABASE CONNECTED");
-        
-        app.listen(PORT, () => {
-            console.log(`Server starting at http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
-};
-
-start();
->>>>>>> 95d72a3a662ffa28d3e5d64044a290121de16b23
