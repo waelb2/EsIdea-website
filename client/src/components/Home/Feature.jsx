f98e2439d1228f84e9185a5eb5feabe468a93f3e
Taleb Mohamed Adnane <lm_taleb@esi.dz>
2024-04-03 12:30:56 +0600

    Improved styling and added popup functionality on button click

--- client/src/components/Home/Feature.jsx
+++ client/src/components/Home/Feature.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Feature = (props) => {
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <img className=' h-40 my-5' src={props.image}/>
-      <p className='my-3 font-medium text-3xl'>{props.title}</p>
-      <p className='text-lg'>{props.description}</p>
+    <div className='flex flex-col md:items-start xs:items-center justify-center gap-1'>
+      <img className='xs:w-24 md:w-32' src={props.image}/>
+      <div>
+        <p className=' font-medium text-lg xs:text-center md:text-start'>{props.title}</p>
+        <p className='text-sm xs:text-center md:text-start'>{props.description}</p>
+      </div>
     </div>
   )
 }
