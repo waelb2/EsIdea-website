7a44f66584022542f4f740902f92ec40216e381a
bouguessa wail <105824672+waelb2@users.noreply.github.com>
2024-04-23 17:16:50 +0100

    packages and routes update

--- server/routes.ts
+++ server/routes.ts
@@ -1,16 +1,16 @@
-import express from "express"
-import userRoutes from "./src/components/user/userRoutes"
-import authRoutes from "./src/components/auth/authRoutes"
-import dashboardRoutes from "./src/components/dashboard/dashboardRoutes"
-import projectRoutes from "./src/components/project/projectRoutes"
-import homeRoutes from "./src/components/home/homeRoutes"
-
+import express from 'express'
+import userRoutes from './src/components/user/userRoutes'
+import authRoutes from './src/components/auth/authRoutes'
+import dashboardRoutes from './src/components/dashboard/dashboardRoutes'
+import projectRoutes from './src/components/project/projectRoutes'
+import homeRoutes from './src/components/home/homeRoutes'
+import invitationRoutes from './src/components/invitation/invitationRoutes'
 const router = express.Router()
 
-router.use("/user", userRoutes)
-router.use("/home", homeRoutes)
-router.use("/auth", authRoutes)
-router.use("/dashboard", dashboardRoutes)
-router.use('/project',projectRoutes)
-
-export default router 
+router.use('/user', userRoutes)
+router.use('/home', homeRoutes)
+router.use('/auth', authRoutes)
+router.use('/dashboard', dashboardRoutes)
+router.use('/project', projectRoutes)
+router.use('/invitation', invitationRoutes)
+export default router
