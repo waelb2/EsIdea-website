e45ff5e312a49adf8744f44c949ba49d9b79ef51
Badis <badisbua@gmail.com>
2024-04-23 10:39:52 +0100

    first small changes

--- server/src/config/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-var mongoose_1 = require("mongoose");
-var connectDB = function (uri) {
-    return mongoose_1.default.connect(uri);
-};
-exports.connectDB = connectDB;
