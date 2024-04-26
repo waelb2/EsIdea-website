c04d5542039952896707d3c22d6f746104e7866c
Badis <badisbua@gmail.com>
2024-04-26 15:47:56 +0100

    Forget to add club/module/event routes

--- server/routes.ts
+++ server/routes.ts
@@ -1,10 +1,16 @@
 import express from "express"
 import userRoutes from "./src/components/user/userRoutes"
 import projectRoutes from "./src/components/project/projectRoutes"
+import clubRoutes from "./src/components/club/clubRoutes"
+import moduleRoutes from "./src/components/module/moduleRoutes"
+import eventRoutes from "./src/components/event/eventRoutes"
 
 const router = express.Router()
 
 router.use("/user", userRoutes)
-router.use('/project',projectRoutes)
+router.use('/project', projectRoutes)
+router.use('/club', clubRoutes)
+router.use('/module', moduleRoutes)
+router.use('/event', eventRoutes)
 
 export default router 
