02e4a29efd4204c8c77d9e944de3be02cde1339e
bouguessa wail <105824672+waelb2@users.noreply.github.com>
2024-04-27 10:33:14 +0100

    nodemailer.ts updated

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -1,24 +1,21 @@
-import * as nodeMailer from "nodemailer";
-
+import * as nodeMailer from 'nodemailer'
 
 export const sendEmail = async (option: any) => {
-    const transporter: nodeMailer.Transporter = nodeMailer.createTransport({
-        service:"gmail",
-        host: process.env.EMAIL_HOST,
+  const transporter: nodeMailer.Transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    host: process.env.EMAIL_HOST,
 
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.USER_PASSWORD,
-
-        },
-    });
-
-
-    const emailOptions = {
-        from: 'from@example.com',
-        to: option.email,
-        subject: option.subject,
-        text: option.message,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.USER_PASSWORD
     }
-    await transporter.sendMail(emailOptions);
+  })
+
+  const emailOptions = {
+    from: 'from@example.com',
+    to: option.email,
+    subject: option.subject,
+    text: option.message
+  }
+  await transporter.sendMail(emailOptions)
 }
