51c2a2d8f1715d968673143662d7ff67e26e6478
bouguessa wail <105824672+waelb2@users.noreply.github.com>
2024-04-27 10:23:58 +0100

    club  updated

--- server/src/components/club/clubControllers.ts
+++ server/src/components/club/clubControllers.ts
@@ -4,9 +4,7 @@ import { Club } from './clubModel'
 const getClubs = async (req: Request, res: Response) => {
   try {
     const clubs = await Club.find({})
-    return res.status(200).json({
-      clubs
-    })
+    return res.status(200).json(clubs)
   } catch (error) {
     console.log(error)
     return res.sendStatus(500)
--- server/src/components/club/clubRoutes.ts
+++ server/src/components/club/clubRoutes.ts
@@ -1,9 +1,9 @@
-import express from "express"
-import { getClubs } from "./clubControllers"
+import express from 'express'
+import { getClubs } from './clubControllers'
+import { authMiddleWare } from '../auth/authMiddleware'
 
-const router =  express.Router()
+const router = express.Router()
 
-
-router.get("/getClubs", getClubs)
+router.get('/getClubs', authMiddleWare, getClubs)
 
 export default router
