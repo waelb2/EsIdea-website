243a2cf357ddb6f5f6795f14d7396103093a88a8
bahaaeddineg <mlbegueroumi@gmail.com>
2024-05-11 23:06:59 +0100

    docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    port:5174,
+     host: true,
+    strictPort : true,
+    port:5174
     // host: "192.168.43.66"
   }
 })
