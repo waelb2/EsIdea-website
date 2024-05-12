eb3a3e114ddbc49573e6d9b4760f6d9cc0b3775c
waelb2 <lw_bouguessa@esi.dz>
2024-05-12 23:36:11 +0100

    update stickers style

--- client/src/components/visualisation/Sticker.jsx
+++ client/src/components/visualisation/Sticker.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const getRandomRotation = () => {
-  // Generate a random rotation angle between -5 and 5 degrees
   return `rotate(${Math.random() * 10 - 5}deg)`;
 };
 
@@ -9,29 +8,15 @@ const Sticker = ({ color, idea }) => {
   const stickerStyle = {
     position: 'relative',
     display: 'inline-block',
-    width: '200px',
+    width: '300px',
     height: 'auto',
     boxShadow: '0 10px 20px rgba(0, 0, 0, 0.3)',
     transform: getRandomRotation(), 
     marginBottom: '20px', 
   };
-
-  const cornerStyle = {
-    position: 'absolute',
-    top: '-20px',
-    right: '-20px',
-    width: '10px',
-    height: '10px',
-    transform: 'rotate(45deg)',
-    background:'#f3f4f6',
-    zIndex: '1',
-      clipPath: 'polygon(100% 0, 100% 100%, 0% 100%)', // Add curvature to the corner
-
-  };
-
+  
   return (
     <div className={` ${color}`} style={stickerStyle}>
-      <div style={cornerStyle} ></div>
       <p
         className="text-xl font-semibold p-4"
         style={{ position: 'relative', zIndex: '2' }}
